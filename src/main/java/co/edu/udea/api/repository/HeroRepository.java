74f97e9e204288d5ad62b5b0f4186ef7b00ec89b
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-10-16 20:06:55 -0500

    7commit modificacion puerto 8181 springbot en pom

--- src/main/java/co/edu/udea/api/repository/HeroRepository.java
+++ src/main/java/co/edu/udea/api/repository/HeroRepository.java
@@ -2,8 +2,14 @@ package co.edu.udea.api.repository;
 
 import co.edu.udea.api.model.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 public interface HeroRepository extends JpaRepository<Hero, Integer> {
+
+    @Query("SELECT e FROM Hero e WHERE e.name LIKE %:nombre%")//agrego esta
+    List<Hero> findByNameContaining(@Param("nombre") String name);//agrego esta
+
 }
